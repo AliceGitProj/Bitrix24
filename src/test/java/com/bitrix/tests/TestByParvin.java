f8da8f2561fbddcd8592ba661c0eb19c5ba60850
pkouliev <pkouliev@gmail.com>
2020-04-10 12:56:24 -0400

    added new test

---
+++ src/test/java/com/bitrix/tests/TestByParvin.java
@@ -0,0 +1,11 @@
+package com.bitrix.tests;
+
+public class TestByParvin {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Another best. Hopefully works!");
+    }
+}
+
