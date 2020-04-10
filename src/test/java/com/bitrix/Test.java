6cd5448d25be51a0ed256275c8aac5bef0bbda7e
pkouliev <pkouliev@gmail.com>
2020-04-10 09:24:51 -0400

    added a new test file

--- src/test/java/com/bitrix/Test.java
+++ src/test/java/com/bitrix/Test.java
@@ -2,6 +2,6 @@ package com.bitrix;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi");
+
     }
 }
---
+++ src/test/java/com/bitrix/TestByParvin.java
@@ -0,0 +1,9 @@
+package com.bitrix;
+
+public class TestByParvin {
+
+
+    public static void main(String[] args) {
+        System.out.println("Good Morning! This is Test");
+    }
+}
