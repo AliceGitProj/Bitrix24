438c486ac30aeddc456f1e7d962961b733547fd9
Shanoza <shaxnoza12014@gmail.com>
2020-04-10 21:36:16 -0400

    added a new class on my local branch

---
+++ src/test/java/com/bitrix/tests/TestNewBranchShaxnoza.java
@@ -0,0 +1,11 @@
+package com.bitrix.tests;
+
+
+public class TestNewBranchShaxnoza{
+
+        public static void main(String[] args) {
+            System.out.println("Test1");
+            System.out.println("added changes on local branch ");
+        }
+
+    }
