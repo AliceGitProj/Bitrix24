bf5fda4b18be95a7bbe1e01d430279e98741e0c3
pkouliev <pkouliev@gmail.com>
2020-04-12 00:45:46 -0400

    LoginTest updated with Data Provider based on Excel spreadsheet

--- src/test/java/com/bitrix/testPackage/LoginTest.java
+++ src/test/java/com/bitrix/testPackage/LoginTest.java
@@ -1,7 +1,7 @@
 package com.bitrix.testPackage;
 
 import com.bitrix.pages.LoginPage;
-import com.bitrix.utilities.BrowserUtilities;
+import com.bitrix.utilities.ExcelUtil;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -9,46 +9,44 @@ import org.testng.annotations.Test;
 public class LoginTest extends AbstractTestBase {
 
 
-    @Test(priority = 0)
+    @Test()
     public void verifySiteLogo() {
 
         LoginPage loginPage = new LoginPage();
 
         test = reports.createTest("Verify Website logo");
-
         Assert.assertEquals(loginPage.getSiteLogoText(), "Authorization", "Website Logo did not match");
 
         test.info("Login as help desk");
-        BrowserUtilities.wait(2);
         Assert.assertEquals(loginPage.getSiteLogoText(), "Authorization");
-        //if assertion passed, it will set test status in report to passed
-
 
         test.pass("Website logo Authorization was verified");
     }
 
+    @Test(dataProvider = "credentialsFromExcel", priority = 1)
+    public void loginTestWithExcel(String usernameValue, String passwordValue) {
 
-    @Test(dataProvider = "credentials", priority = 1)
-    public void loginWithDDT(String usernameValue, String passwordValue) {
         test = reports.createTest("Verify page logo as" + usernameValue);
+
         LoginPage loginPage = new LoginPage();
         loginPage.login(usernameValue, passwordValue);
+
         test.info("Login as " + usernameValue);
-        BrowserUtilities.wait(2);
         Assert.assertEquals(loginPage.getPageLogoText(), "CRM");
+
         test.pass("Page logo CRM was verified");
+        test = reports.createTest("Login test for username " + usernameValue);
     }
 
     @DataProvider
-    public Object[][] credentials() {
-        return new Object[][]{
-                {"helpdesk49@cybertekschool.com", "UserUser"},
-                {"helpdesk50@cybertekschool.com", "UserUser"},
-                {"hr49@cybertekschool.com", "UserUser"},
-                {"hr50@cybertekschool.com", "UserUser"},
-                {"marketing49@cybertekschool.com", "UserUser"},
-                {"marketing50@cybertekschool.com", "UserUser"}
-        };
+    public Object[][] credentialsFromExcel() {
+
+        String path = "BitrixCredentials.xlsx";
+        String spreadSheet = "Credentials";
+
+        ExcelUtil excelUtil = new ExcelUtil(path, spreadSheet);
+        return excelUtil.getDataArray();
     }
+
 
 }
